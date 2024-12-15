75a408ad4396072fc2e920b9ddcc06ddb3d097a9
LYChubby <f4uzi345@gmail.com>
2024-12-15 22:18:12 +0700

    Membuat Fungsi Untuk Mengambil Dan Menampilan Detail Mahasiswa

--- app/src/main/java/com/example/mvvmdatabase/viemodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/viemodel/DetailMhsViewModel.kt
@@ -1,6 +1,19 @@
 package com.example.mvvmdatabase.viemodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+import com.example.mvvmdatabase.repository.RepositoryMhs
+import com.example.mvvmdatabase.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
@@ -28,4 +41,43 @@ fun Mahasiswa.toDetailUiState(): MahasiswaEvent {
         kelas = kelas,
         angkatan = angkatan
     )
+}
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    val detailUiState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState(
+                detailUiEvent = it.toDetailUiState(),
+                isLoadings = false,
+            )
+        }
+        .onStart {
+            emit(DetailUiState(isLoadings = true))
+            delay(600)
+        }
+        .catch {
+            emit(
+                DetailUiState(
+                    isLoadings = false,
+                    isError = true,
+                    errorMessages = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState(
+                isLoadings = true,
+            ),
+        )
+
+
 }
