94929aee2e1f4f900ee79af0c96d38b7e0452c3d
LYChubby <f4uzi345@gmail.com>
2024-12-15 22:10:24 +0700

    Membuat Fungsi Untuk Memindahkan Data Dari Entity Ke UI

--- app/src/main/java/com/example/mvvmdatabase/viemodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/viemodel/DetailMhsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.mvvmdatabase.viemodel
 
+import com.example.mvvmdatabase.data.entity.Mahasiswa
+
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoadings: Boolean = false,
@@ -11,4 +13,19 @@ data class DetailUiState(
 
     val isUiEventNotEmpty: Boolean
     get() = detailUiEvent != MahasiswaEvent()
+}
+
+// Data Class Untuk Menampung Data Yang Akan Ditampilkan Di UI
+
+// Memindahkan Data Dari Entity Ke UI
+
+fun Mahasiswa.toDetailUiState(): MahasiswaEvent {
+    return MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
