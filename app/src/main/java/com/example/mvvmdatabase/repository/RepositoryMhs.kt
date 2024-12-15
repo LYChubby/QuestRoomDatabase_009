c36ba8d55b3471a557aab99de3c243506a705b5b
LYChubby <f4uzi345@gmail.com>
2024-12-15 21:39:09 +0700

    Menambah Fungsi getAllMhs Untuk Mendapatkan Data

--- app/src/main/java/com/example/mvvmdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/mvvmdatabase/repository/RepositoryMhs.kt
@@ -1,10 +1,11 @@
 package com.example.mvvmdatabase.repository
 
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
     suspend fun updateMhs(mahasiswa: Mahasiswa)
     suspend fun deleteMhs(mahasiswa: Mahasiswa)
-
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
