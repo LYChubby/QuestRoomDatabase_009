935014e5f3acaaa2446c1942205954838f637ca3
LYChubby <f4uzi345@gmail.com>
2024-12-15 22:19:30 +0700

    Membuat Fungsi Delete

--- app/src/main/java/com/example/mvvmdatabase/viemodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/mvvmdatabase/viemodel/DetailMhsViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
@@ -79,5 +80,11 @@ class DetailMhsViewModel (
             ),
         )
 
-
+    fun deleteMhs() {
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
 }
