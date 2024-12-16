f53dcdfe46e1b0e0723566dcf78f1da381ea30a7
LYChubby <f4uzi345@gmail.com>
2024-12-16 20:58:05 +0700

    Membuat Fungsi Untuk Body

--- app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,8 +1,10 @@
 package com.example.mvvmdatabase.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,16 +14,23 @@ import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mvvmdatabase.data.entity.Mahasiswa
+import com.example.mvvmdatabase.viemodel.HomeUiState
+import kotlinx.coroutines.launch
 
 @OptIn (ExperimentalMaterial3Api::class)
 @Composable
@@ -106,5 +115,64 @@ fun ListMahasiswa(
                 )
             }
         )
+    }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan Indikator Loading
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            // Menampilkan Snackbar jika terjadi kesalahan
+            LaunchedEffect(homeUiState.errorMessages) {
+                homeUiState.errorMessages?.let { message ->
+                    coroutineScope.launch {
+                        snackBarHostState.showSnackbar(message) // Tampilkan Snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isEmpty() -> {
+            // Menampilkan pesan jika daftar mahasiswa kosong
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak Ada Data Mahasiswa",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar mahasiswa jika tidak ada kesalahan
+            ListMahasiswa(
+                listMhs = homeUiState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = modifier
+            )
+        }
     }
 }
