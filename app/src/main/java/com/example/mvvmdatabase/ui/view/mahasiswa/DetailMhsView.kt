b7f62b5bbe951bc4394e6d596a424a8f43902197
LYChubby <f4uzi345@gmail.com>
2024-12-16 21:28:28 +0700

    Membuat ComponentDetailMhs

--- app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/mvvmdatabase/ui/view/mahasiswa/DetailMhsView.kt
@@ -1,11 +1,17 @@
 package com.example.mvvmdatabase.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.mvvmdatabase.R
 
 @Composable
@@ -30,4 +36,29 @@ private fun DeleteConfirmationDialog(
             }
         }
     )
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ){
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
 }
